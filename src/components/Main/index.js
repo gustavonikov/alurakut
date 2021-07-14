3b68db07ae4ce15556ea99e3b2760173a63bcfa0
Gustavo <gustavonikov@gmail.com>
2021-07-14 00:58:28 -0300

    add: Main component

---
+++ src/components/Main/index.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+export const Main = styled.main`
+    width: 100%;
+    max-width: 500px;
+    margin-left: auto;
+    margin-right: auto;
+    padding: 16px;
+    grid-gap: 10px;
+
+    .profile {
+    display: none;
+
+    @media (min-width: 860px) {
+        display: block;
+    }
+    }
+
+    @media (min-width: 860px) {
+    max-width: 1110px;
+    display: grid;
+    grid-template-areas: 
+        'profile welcome profileRelations';
+    grid-template-columns: 160px 1fr 312px;
+    }
+`
