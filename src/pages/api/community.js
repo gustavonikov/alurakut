ee2fa01ef39483ec9a92735fbc360b2587a5777f
Gustavo <gustavonikov@gmail.com>
2021-07-18 01:57:09 -0300

    add: communities api request to datocms

---
+++ src/pages/api/community.js
@@ -0,0 +1,24 @@
+import { SiteClient } from 'datocms-client'
+
+export default async function requestsReceptor(req, res) {
+    if (req.method === 'POST') {
+        const client = new SiteClient(process.env.FULL_API_TOKEN)
+
+        const communityRecord = await client.items.create({
+            itemType: '975865',
+            ...req.body
+        })
+
+        res.json({
+            data: 'some data',
+            communityRecord
+        })
+
+        return
+    }
+
+    return res.status(404).json({
+        status: 404,
+        message: 'Not available to make a GET request yet.'
+    })
+}
