4ac8b562b4005e7a7c0884113c52129984bc47f5
Gustavo <gustavonikov@gmail.com>
2021-07-19 02:48:44 -0300

    add: scraps component

---
+++ src/components/AppInteractions/ScrapsForm.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export default function ScrapsForm() {
+    return (
+        <h1>Scraps</h1>
+    )
+}
---
+++ src/pages/scraps.js
@@ -0,0 +1,6 @@
+
+export default function Scraps() {
+    return (
+        <h1>Hello scraps!</h1>
+    )
+}
