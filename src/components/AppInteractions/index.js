1942ab9c71dd7407c82de1b408816f4f2d6244ec
Gustavo <gustavonikov@gmail.com>
2021-07-19 03:43:40 -0300

    refactor: forms

--- src/components/AppInteractions/index.js
+++ src/components/AppInteractions/index.js
@@ -1,8 +1,6 @@
 import { useState } from 'react';
 import styled, { css } from 'styled-components'
-import CommunityForm from './CommunityForm'
-import ScrapsForm from './ScrapsForm';
-import TestimonialsForm from './TestimonialsForm';
+import Form from './Form';
 
 const AppInteractionsWrapper = styled.div`
     display: flex;
@@ -55,18 +53,49 @@ export default function AppInteractions({ handleCommunitySubmit, handleTestimoni
                     onClick={() => { setActiveBtn('scraps') }}
                     active={activeBtn}
                 >
-                    Deixar um scrap
+                    Deixar um recado
                 </AppInteractionsButton>
             </AppInteractionsWrapper>
             {
-                activeBtn === 'community' && <CommunityForm handleSubmit={handleCommunitySubmit} />
-            } 
+                activeBtn === 'community' &&
+                <Form
+                    formId="community-form"
+                    handleSubmit={handleCommunitySubmit}
+                    firstElName="community-title"
+                    secondElName="community-image"
+                    firstPlaceholder="Qual vai ser o nome da sua comunidade?"
+                    secondPlaceHolder="Coloque uma URL para usarmos de capa"
+                    textElement="input"
+                />
+            }
             {
-                activeBtn === 'testimonials' && <TestimonialsForm handleSubmit={handleTestimonialSubmit} />
-            } 
+                activeBtn === 'testimonials' &&
+                <Form
+                    formId="testimonials-form"
+                    handleSubmit={handleTestimonialSubmit}
+                    firstElName="testimonial-author"
+                    secondElName="testimonial-text"
+                    firstPlaceholder="Digite seu usuário do github"
+                    secondPlaceHolder="Digite seu depoimento..."
+                    textElement="textarea"
+                />
+            }
             {
-                activeBtn === 'scraps' && <ScrapsForm handleSubmit={handleCommunitySubmit} />
-            } 
+                activeBtn === 'scraps' &&
+                <Form
+                    formId="scraps-form"
+                    handleSubmit={(ev) => {
+                        ev.preventDefault()
+                        alert('This functionality it\'s not working yet')
+                        document.querySelector('#scraps-form').reset()
+                    }}
+                    firstElName="scrap-author"
+                    secondElName="scrap-text"
+                    firstPlaceholder="Digite seu usuário do github"
+                    secondPlaceHolder="Deixe aqui o seu recado..."
+                    textElement="textarea"
+                />
+            }
         </>
     )
 }
