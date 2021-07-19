e00bd3711d26b40507cfded0c0daa0f803de0f80
Gustavo <gustavonikov@gmail.com>
2021-07-19 02:45:54 -0300

    refactor: updating form element

---
+++ src/components/AppInteractions/Form.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+
+    button[type='submit'] {
+        align-self: center;
+    }
+`
+
+export default Form
